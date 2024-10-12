35ec743d8c60fab37e1167cd25a768a11f0fea5f
Mazhar Altincay <mazharaltincay89@gmail.com>
2024-10-12 15:50:47 +0200

    initial and final set up for my project

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    static{
+
+    }
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        double[] values = new double[]{3.71, 3.76, 3.70, 3.69, 3.64};
+        System.out.println(main.variance(values));
+
+    }
+    public double variance(double[] dataSet){
+        double sumOfArray = 0;
+        for(double val : dataSet)
+            sumOfArray += val;
+
+        // we gon subtract the mean value from every element of the given data set.
+        // Then taking the power of that values and sum everyone then we divide it by length-1
+        double meanVal = sumOfArray / dataSet.length;
+
+        double sumOfPoweredNumbers = 0;
+        for(double val : dataSet){
+            sumOfPoweredNumbers += Math.pow((val - meanVal), 2);
+        }
+
+        return sumOfPoweredNumbers / (dataSet.length - 1);
+    }
+}
